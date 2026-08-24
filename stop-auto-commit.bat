b1950fdcb4db745790488345a06ea81a11a339a7
judithamuda-dotcom <judithamuda@gmail.com>
2026-08-24 16:46:41 +0000

    Fix authentication routing and clean submission files

--- stop-auto-commit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Signals the auto-commit watcher to stop gracefully.
-setlocal
-type nul > "%~dp0commit-watch.stop"
-echo Stop signal sent. The watcher will exit within ~30 seconds.
-endlocal
